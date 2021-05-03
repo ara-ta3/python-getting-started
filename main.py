0badddf97a7239007ab163830485a125b1f09d98
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2021-05-03 17:09:15 +0900

    Fix for lint

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from urllib import parse
 
 def main():
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('-a', '--aaa',type=str)
-    arg_parser.add_argument('-b', '--bbb',type=int)
+    arg_parser.add_argument('-a', '--aaa', type=str)
+    arg_parser.add_argument('-b', '--bbb', type=int)
     args = arg_parser.parse_args()
     print(args)
 
